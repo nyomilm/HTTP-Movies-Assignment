afa3a2266ccd11df4c9cbafc2fcb34aab77d44b3
nyomilm <nyomilm@hotmail.com>
2019-10-10 22:50:42 -0500

    1st update

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,64 @@ import './index.css';
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom';
 
+const App = () => {
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/movies/${id}`)
+      .then(res => setMovie(res.data))
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <div className="App">
+      <nav>
+        <h1 className="store-header">New Movie</h1>
+        <div className="nav-links">
+          <NavLink exact to="/item-form">
+            Add Item
+          </NavLink>
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+          <NavLink to="/item-list">Shop</NavLink>
+        </div>
+      </nav>
+
+      <Route exact path="/" component={Home} />
+      <Route
+        exact
+        path="/item-list"
+        render={props => (
+          <ItemsList
+            {...props} // this is the same as below
+            //               match={props.match}
+            //               history={props.history}
+            //               location={props.location}
+            items={items}
+          />
+        )}
+      />
+      <Route
+        path="/item-list/:id"
+        render={props => (
+          <Item {...props} items={items} updateItems={setItems} />
+        )}
+      />
+      <Route
+        path="/update-item/:id"
+        render={props => (
+          <UpdateForm {...props} items={items} updateItems={setItems} />
+        )}
+      />
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
 ReactDOM.render(
   <Router>
     <App />
   </Router>,
-  document.getElementById('root')
+  rootElement
 );
